08816ec62d914ab62c724d676e79a4fb090a4116
Fatih Kilic <me@fklc.dev>
2024-06-06 12:16:14 -0400

    Implement automated builds and polish

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,10 +18,29 @@ export default defineConfig({
         }
       },
     },
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/api.mjs",
+          dest: "",
+        },
+        {
+          src: "src/manifest.json",
+          dest: "",
+        },
+        {
+          src: "src/schema.json",
+          dest: "",
+        },
+      ],
+    }),
   ],
   build: {
     outDir: "build",
     sourcemap: true,
+    rollupOptions: {
+      external: ["src/manifest.json"],
+    },
   },
   base: "",
 });
