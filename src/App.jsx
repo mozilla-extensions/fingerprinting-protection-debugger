e25eaba64d62839d9010480bb30c7bf90f4a6a65
Fatih Kilic <me@fklc.dev>
2024-07-19 12:18:48 -0400

    Export ReadinessChecker to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import PropTypes from "prop-types";
-import { useEffect } from "react";
 import BlockingMessage from "./components/BlockingMessage";
+import Notifications from "./components/Notifications";
+import ReadinessChecker from "./components/ReadinessChecker";
 import SearchBox from "./components/SearchBox";
 import SetAllButtons from "./components/SetAllButtons";
 import TargetList from "./components/TargetList";
 import Troubleshooter from "./components/Troubleshooter";
 import useStore from "./state";
-import Notifications from "./components/Notifications";
 
 export default function App() {
   const [blockingMessage] = useStore((state) => [
@@ -46,65 +46,3 @@ Layout.propTypes = {
     PropTypes.node,
   ]).isRequired,
 };
-
-function ReadinessChecker() {
-  const [targets, troubleshooter, setBlockingMessage, notifications] = useStore(
-    (state) => [
-      state.targets,
-      state.troubleshooter,
-      state.blockingMessage.set,
-      state.notifications,
-    ]
-  );
-
-  // Load the targets and troubleshooter data
-  useEffect(
-    () => {
-      if (!targets.loaded) targets.load();
-      if (!troubleshooter.loaded) troubleshooter.load();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
-
-  const loaded = targets.loaded && troubleshooter.loaded;
-  // Show a blocking message while the app is loading
-  useEffect(() => {
-    if (!loaded) {
-      setBlockingMessage("Loading...");
-      return;
-    }
-
-    setBlockingMessage("");
-  }, [loaded, setBlockingMessage]);
-
-  useEffect(() => {
-    if (!loaded) return;
-
-    // Show a notification if fingerprinting protection is not enabled
-    if (!targets.enabled) {
-      const id = "fpp-not-enabled";
-      notifications.add({
-        id,
-        message: "Fingerprinting protection is not enabled!",
-        action: async () => {
-          await targets.enable();
-          notifications.remove(id);
-        },
-        actionLabel: "Enable",
-      });
-    }
-
-    // Show a notification if unsupported targets are found
-    if (targets.invalid.length !== 0) {
-      notifications.add({
-        id: "unsupported-targets",
-        message: `Unsupported targets were found (${
-          " " + targets.invalid.join(", ")
-        }). The extension will erase them when you make changes.`,
-      });
-    }
-  }, [loaded, notifications, targets]);
-
-  return null;
-}
