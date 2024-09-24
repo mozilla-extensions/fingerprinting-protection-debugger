528c330601031ef9771eff0bba211c29da053850
Fatih Kilic <me@fklc.dev>
2024-09-24 15:49:44 -0400

    Show warning if the user has a granular override for the website

--- src/store.ts
+++ src/store.ts
@@ -111,6 +111,18 @@ export default create<StateType>()(
           state.targets.global = global;
           state.targets.granular = granular;
           state.activeTab.domain = domain;
+
+          // We currently don't expose the granular override support.
+          // Add a notification if granular overrides are found.
+          if (Object.keys(granular).length !== 0) {
+            state.notifications.list.push({
+              id: "granular-overrides",
+              message:
+                "Granular overrides are found. The extension does not support them yet.",
+              action: () => get().notifications.remove("granular-overrides"),
+              actionLabel: "Dismiss",
+            });
+          }
         });
       },
     },
