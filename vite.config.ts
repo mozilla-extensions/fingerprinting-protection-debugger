d159d792aaf7734eb1ebaadc0408161f843d146a
Fatih Kilic <me@fklc.dev>
2025-06-03 11:47:19 -0400

    Update vulnerable deps and remove vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,41 +9,18 @@ export default defineConfig({
       name: "hot-reload-exp-api",
       async buildStart() {
         for (const file of [
-          "src/api.mjs",
-          "src/manifest.json",
-          "src/schema.json",
+          "public/api.mjs",
+          "public/manifest.json",
+          "public/schema.json",
         ]) {
           this.addWatchFile(file);
         }
       },
     },
-    viteStaticCopy({
-      targets: [
-        {
-          src: "src/api.mjs",
-          dest: "",
-        },
-        {
-          src: "src/manifest.json",
-          dest: "",
-        },
-        {
-          src: "src/schema.json",
-          dest: "",
-        },
-        {
-          src: "assets/fingerprint.svg",
-          dest: "assets",
-        },
-      ],
-    }),
   ],
   build: {
     outDir: "build",
     sourcemap: true,
-    rollupOptions: {
-      external: ["src/manifest.json"],
-    },
   },
   base: "",
 });
